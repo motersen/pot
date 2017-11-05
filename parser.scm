motersen/pot

commit 45fe8f22becec065f374575e3bac94ebc43c7b65

tokenize: cons-reverse instead of append
Make Code easier to read and improve performance.

parser.scm
@@ -40,18 +40,17 @@
 	(let tokenize ((s (string->list s))
 								 (tokens (list)))
 		(if (null? s)
-				tokens
+				(reverse tokens)
 				(let ((c (car s)))
 					(cond
 					 ((tag-char? c)
 						(let ((tag.rest (collect-tag s)))
-							(tokenize (cdr tag.rest) (append tokens (list (car tag.rest))))))
+							(tokenize (cdr tag.rest) (cons (car tag.rest) tokens))))
 					 ((op? c)
-						(tokenize (cdr s)
-											(append tokens (list (list->string (list c))))))
+						(tokenize (cdr s) (cons (list->string (list c)) tokens)))
 					 ((char=? #\( c)
 						(let ((group.rest (tokenize (cdr s) (list))))
-							(tokenize (cdr group.rest) (append tokens (list (car group.rest))))))
+							(tokenize (cdr group.rest) (cons (car group.rest) tokens))))
 					 ((char=? #\) c)
 						(cons tokens (cdr s)))
 					 (#t

commit ad55694e2b0566e1b9511dd02fae34b03bd6624d

Fix reversed tokens in parentheses
Fix a bug introduced in:
45fe8f22bece tokenize: cons-reverse instead of append

The tokenizer procedure was transformed to cons new tokens to the front of the
token-list and reverse it when returning. Because the recursive application of
the tokenizer when processing filters contained in parentheses did not reverse
the list when returning, the parts of filters contained in parentheses would be
reversed.

Simply reversing the list in the base case processing a closing parenthesis
resolves the issue.

parser.scm
@@ -50,7 +50,7 @@
 						(let ((group.rest (tokenize (cdr s) (list))))
 							(tokenize (cdr group.rest) (cons (car group.rest) tokens))))
 					 ((char=? #\) c)
-						(cons tokens (cdr s)))
+						(cons (reverse tokens) (cdr s)))
 					 (#t
 						(shout (string-append
 										"Not a valid symbol: '"